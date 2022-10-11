714b27bdd01f7f06f2ad51364a0417205b8feb52
Mohammad Sumon <sumon3011@gmail.com>
2022-10-11 12:46:32 +0600

    toast added successfully

--- src/components/AllQuestionDetails.js
+++ src/components/AllQuestionDetails.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import AnswerOption from './AnswerOption';
+import { EyeIcon } from '@heroicons/react/24/solid';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const AllQuestionDetails = ({qs}) => {
     console.log(qs);
     const {options, question} = qs;
+
+    const notify = () => toast(qs.correctAnswer);
+    
     return (
+        
         <div className='border-solid border-2 border-orange-200 my-6'>
+            <div className='flex justify-center items-center px-4'>
             <p className='text-2xl'>Question: {question}</p>
+            <EyeIcon onClick={notify} className="h-6 w-6 text-orange-300 ml-4 cursor-pointer"/>
+            <ToastContainer position="top-center" autoClose={2000}></ToastContainer>
+            </div>
+            
             {
                 options.map(ans => <AnswerOption key={ans.id} ans={ans}></AnswerOption>)
             }
--- src/components/AnswerOption.js
+++ src/components/AnswerOption.js
@@ -5,10 +5,10 @@ const AnswerOption = ({ans}) => {
     console.log(ans);
     return (
         <div>
-            <div className=' items-center text-center'>
+            <div className='items-center text-center'>
             
             <p className='bg-orange-100 m-4 p-4 rounded-md cursor-pointer hover:bg-orange-300'>
-            <StopCircleIcon className="h-6 w-6 text-orange-300 hover:text-black"/>
+            <StopCircleIcon className="h-6 w-6 text-orange-300"/>
                 {ans}</p>
         </div>
         </div>
